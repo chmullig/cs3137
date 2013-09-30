287a330f525410006794f04a47ca563ba9638fcd
Chris Mulligan <clm2186@columbia.edu>
2013-09-30 10:28:49 -0400

    Finished Fibonacci numbers.

--- theory1/fibtimes.R
+++ theory1/fibtimes.R
@@ -1,4 +1,7 @@
 library(ggplot2)
-df <- read.table("fibonacci.cleantimes")
-qplot(data=df, x=n, y=time, color=type, geom="line", xlab="n", ylab="time (seconds)")
+df <- read.table("fibonacci.cleantimes", header=TRUE)
+qplot(data=df, x=n, y=time, color=algorithm, geom="line", xlab="n", ylab="time (seconds)")
 ggsave("fibtimes.png", height=3.5, width=6)
+
+library(reshape2)
+print(dcast(df, n ~ algorithm))

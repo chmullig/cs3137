dfaa43a965d9800ce9a3860b361a378fe2695b52
Chris Mulligan <clm2186@columbia.edu>
2013-09-30 21:03:33 -0400

    theory 1, q5

--- theory1/fibtimes.R
+++ theory1/fibtimes.R
@@ -4,4 +4,4 @@ qplot(data=df, x=n, y=time, color=algorithm, geom="line", xlab="n", ylab="time (
 ggsave("fibtimes.png", height=3.5, width=6)
 
 library(reshape2)
-print(dcast(df, n ~ algorithm))
+write.table(dcast(df[df$n %in% c(10, 20, 30, 35, 40, 45, 50, 51),], algorithm ~ n), quote=FALSE, sep=" & ", row.names=FALSE, eol= "\\\\\n\\hline\n")

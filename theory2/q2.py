ee854eebcbd6b17c1527d0cf2624310222b75f04
Chris Mulligan <clm2186@columbia.edu>
2013-10-20 14:56:58 -0400

    Theory 2: q2

---
+++ theory2/q2.py
@@ -0,0 +1,21 @@
+stack = []
+
+stack.append(15)
+stack.append(28)
+print stack.pop(), "\t#Stack:", stack
+stack.append(3)
+stack.append(81)
+print stack.pop(), "\t#Stack:", stack
+print stack.pop(), "\t#Stack:", stack
+stack.append(9)
+stack.append(1)
+print stack.pop(), "\t#Stack:", stack
+stack.append(7)
+stack.append(4)
+print stack.pop(), "\t#Stack:", stack
+print stack.pop(), "\t#Stack:", stack
+stack.append(4)
+print stack.pop(), "\t#Stack:", stack
+
+
+print "Stack:", stack

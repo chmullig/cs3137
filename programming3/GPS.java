d9b521746e623e63cd503a32f3c78ce7fcac7440
Chris Mulligan <clm2186@columbia.edu>
2013-12-06 17:10:22 -0500

    Starting on prog 3

---
+++ programming3/GPS.java
@@ -0,0 +1,12 @@
+
+public class GPS {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ programming3/README.md
@@ -0,0 +1,7 @@
+Chris Mulligan
+clm2186@columbia.edu
+COMSW3137 Data Structures & Algorithms
+Hershkop, Fall 2013
+
+Programming 3
+=============
